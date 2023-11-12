84f791d6f898d06e30733b884e80819cec9c90ca
DESKTOP-HH8P8VJ\陳政傑 <sa0921m3@gmail.com>
2023-11-12 15:30:37 +0800

    1.更新角色移動程式

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,11 +11,29 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 8f;
     public float crouchSpeedDivisor = 3f;
 
+    [Header("¸õÅD°Ñ¼Æ")]
+    public float jumpForce = 6.3f;
+    public float jumpHoldForce = 1.9f;
+    public float jumpHoldDuration = 0.1f;
+    public float crouchJumpBoost = 2.5f;
+
+    float jumpTime;
+
     [Header("ª¬ºA")]
     public bool isCrouch;
+    public bool isOnGround;
+    public bool isJump;
+
+    [Header("Àô¹ÒÀË´ú")]
+    public LayerMask groundLayer;
 
     float xVelocity;
 
+    //«öÁä³]¸m
+    public bool jumpPressed;
+    public bool jumpHeld;
+    public bool crouchHeld;
+    
     //¸I¼²Åé¤Ø¤o
     Vector2 colliderStandSize;
     Vector2 colliderStandOffset;
@@ -38,19 +56,31 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        jumpPressed = Input.GetButtonDown("Jump");
+        jumpHeld = Input.GetButton("Jump");
+        crouchHeld = Input.GetButton("Crouch");
     }
 
     private void FixedUpdate()
     {
+        PhysicsCheck();
         GroundMovement();
+        MidAirMovement();
     }
+
+    void PhysicsCheck() 
+    {
+        if(coll.IsTouchingLayers(groundLayer))
+            isOnGround = true;
+        else isOnGround = false;
+    }
+
 
     void GroundMovement() 
     {
-        if (Input.GetButton("Crouch"))
+        if (crouchHeld)
             Crouch();
-        else if (!Input.GetButton("Crouch") && isCrouch)
+        else if (!crouchHeld && isCrouch)
             StandUp();
 
         xVelocity = Input.GetAxis("Horizontal"); //-1f~1f
@@ -64,6 +94,28 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
+    void MidAirMovement() 
+    {
+        if(jumpPressed && isOnGround && !isJump) 
+        {
+
+            isOnGround = false;
+            isJump = true;
+
+            jumpTime = Time.time + jumpHoldDuration;
+
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+        else if (isJump) 
+        {
+            if (jumpHeld)
+                rb.AddForce(new Vector2(0f, jumpHoldForce), ForceMode2D.Impulse);
+            if(jumpTime < Time.time)
+                isJump = false;
+        }
+
+    }
+    
     void FilpDirction()
     {
         if (xVelocity < 0)
